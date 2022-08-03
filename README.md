8fafebb9da7d68c5010368ba784fb34f08561c16
shiftinv <me@shiftinv.cc>
2022-08-04 01:24:44 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# sphinxcontrib-uwu
+For sphinx projects that have a severe lack of uwu.
+
+Why? Yes.  
+<sup><sub>I wanted a pet project for playing around with sphinx extensions, this is essentially it.</sub></sup>
+
+
+## Usage
+Not sure if this'll ever see the light of day and be uploaded to pypi,
+I hope no-one actually uses this (why would you?).
+
+In any case, add `sphinxcontrib_uwu` to the `extensions` list in your `conf.py`, preferably to the end because of how things work, otherwise it's not unlikely that nothing will happen.
+
+
+## Configuration
+No.
---
+++ sphinxcontrib_uwu/__init__.py
@@ -0,0 +1,75 @@
+from __future__ import annotations
+
+import random
+import re
+from typing import Any
+
+from docutils import nodes
+from docutils.writers.html5_polyglot import HTMLTranslator as BaseTranslator
+from sphinx.application import Sphinx
+from sphinx.writers.html import HTMLTranslator
+
+__version__ = "0.0.1"
+
+
+_map = [
+    (re.compile(expr), res)
+    for expr, res in [
+        (r"[lr]", "w"),
+        (r"[RL]", "W"),
+        (r"n([aeiou])", r"ny\1"),
+        (r"N([aeiou])", r"Ny\1"),
+        (r"ove", "uv"),
+    ]
+]
+
+
+def uwuify(text: str) -> str:
+    # replace stuff
+    for regex, res in _map:
+        text = regex.sub(res, text)
+
+    # add stutters
+    words = text.split(" ")
+    for i, word in enumerate(words):
+        if not word or not word[0].isalpha():
+            continue
+        if random.random() > 0.05:
+            continue
+        word = (f"{word[0]}-" * random.randrange(1, 3)) + word
+        words[i] = word
+
+    return " ".join(words)
+
+
+def create_translator(base: type[BaseTranslator]) -> type[BaseTranslator]:
+    class UwuHTMLTranslator(base):
+        def visit_Text(self, node: nodes.Text) -> None:
+            if getattr(self, "protect_literal_text", None):
+                return super().visit_Text(node)
+
+            # intentionally not unescaping since the
+            # node needs to be reconstructed later
+            text = str(node)
+
+            text = uwuify(text)
+
+            # this loses other attached node data like the document,
+            # but I hope no translator needs that
+            return super().visit_Text(nodes.Text(text))
+
+    return UwuHTMLTranslator
+
+
+def setup(app: Sphinx) -> dict[str, Any]:
+    # inherit old translator if possible (yay)
+    old_translator = app.registry.translators.get("html") or HTMLTranslator
+    assert issubclass(old_translator, BaseTranslator)
+    translator_type = create_translator(old_translator)
+    app.set_translator("html", translator_type, override=True)
+
+    return {
+        "version": __version__,
+        "parallel_read_safe": True,
+        "parallel_write_safe": True,
+    }
